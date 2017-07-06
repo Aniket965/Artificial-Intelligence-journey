40248e7085dcc48c2811aa50fca0302c88593370
aniket965 <aniket965.as@gmail.com>
2017-07-06 14:05:47 +0530

    mnist !

--- tensorflow/officialdocs/mnist.py
+++ tensorflow/officialdocs/mnist.py
@@ -1,14 +1,37 @@
+import tensorflow as tf
+
 from tensorflow.examples.tutorials.mnist import input_data
+
 mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
-x = tf.placeholder(tf.float32,[None,784])
 
-W = tf.Variable(tf.zeros([784,10]))
+x = tf.placeholder(tf.float32, [None, 784])
+W = tf.Variable(tf.zeros([784, 10]))
 b = tf.Variable(tf.zeros([10]))
+y = tf.matmul(x, W) + b
+
+# Define loss and optimizer
+y_ = tf.placeholder(tf.float32, [None, 10])
 
 
-# softmax - function for forcing to make probablity to 1
-# activator are used to provide non linearity
-# some activator functions - 
-# identity function 
-# binary step function postive input is 1
-## where as negative input is 0
+#
+#   tf.reduce_mean(-tf.reduce_sum(y_ * tf.log(tf.nn.softmax(y)),
+#                                 reduction_indices=[1]))
+
+
+cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+train_step = tf.train.GradientDescentOptimizer(0.5).minimize(cross_entropy)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+# Train
+for _ in range(1000):
+    batch_xs, batch_ys = mnist.train.next_batch(100)
+    sess.run(train_step, feed_dict={x: batch_xs, y_: batch_ys})
+
+# Test trained model
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+print(sess.run(accuracy, feed_dict={x: mnist.test.images,
+                                    y_: mnist.test.labels}))
